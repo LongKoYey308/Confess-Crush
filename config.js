fe4eaeced43aa110517d939e97550bd24ae02521
LongKoYey308 <91783255+LongKoYey308@users.noreply.github.com>
2021-10-02 17:41:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "xin chao quy ban va cac vi",
+    introTitle: 'Huong',
+    introDesc: `danh nhau khong nek`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'danh nhau ik',
+    desc: 'danh nhau nhe ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
